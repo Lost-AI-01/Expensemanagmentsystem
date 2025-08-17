889af3be66158c9e49cc27a9fc7f1fc10ff3af66
Lost-AI-01 <lost615793@gmail.com>
2025-08-17 18:37:57 +0530

    added new file name home page

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a className="navbar-brand" href="#">Hidden brand</a>
+      <a className="navbar-brand" href="#">Expense Management system</a>
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
---
+++ client/src/components/layout/Layouts.js
@@ -0,0 +1,15 @@
+import React, { Children } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layouts = ({Children}) => {
+    return(
+        <>
+            <Header />
+            <div className="content">{Children}</div>
+            <Footer />
+        </>
+    );
+};
+
+export default Layouts;
