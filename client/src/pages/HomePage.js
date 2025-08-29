2b32049f86bff14f21721aeac884578e3c9716af
Lost-AI-01 <lost615793@gmail.com>
2025-08-29 22:28:55 +0530

    added filter and it's form successfully still some issues occured

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Form, Modal, Select} from "antd";
+import {Button, Form, Modal, Select, Input} from "antd";
 import Layouts from "./../components/layout/Layouts";
 import Footer from "../components/layout/Footer";
 
@@ -28,7 +28,7 @@ const HomePage = () => {
          footer={false}>
             <Form layout="vertical" onFinish={handleSubmit}>
                 <Form.Item label="Amount" name="amount">
-                    <input type="text"/>
+                    <Input placeholder="Enter amount" />
                 </Form.Item>
                  <Form.Item label="Type" name="type">
                     <Select>
@@ -38,10 +38,34 @@ const HomePage = () => {
                 </Form.Item>
                 <Form.Item label="Category" name="category">
                     <Select>
-                        <Select.Option value="Income">Income</Select.Option>
-                        <Select.Option value="Expense">Expense</Select.Option>
+                        <Select.Option value="Salary">Salary</Select.Option>
+                        <Select.Option value="business">Business</Select.Option>
+                        <Select.Option value="investments">Investments</Select.Option>
+                        <Select.Option value="gifts">Gifts</Select.Option>
+                        <Select.Option value="housing">Housing</Select.Option>
+                        <Select.Option value="food">Food</Select.Option>
+                        <Select.Option value="Transport">Transport</Select.Option>
+                        <Select.Option value="healthcare">Healthcare</Select.Option>
+                        <Select.Option value="education">Education</Select.Option>
+                        <Select.Option value="entertainments">Entertainments</Select.Option>
+                        <Select.Option value="loans">Loans</Select.Option>
+                        <Select.Option value="others">Others</Select.Option>
                     </Select>
                 </Form.Item>
+                 <Form.Item label="References" name="reference">
+                    <Input placeholder="Enter References " />
+                </Form.Item>
+                <Form.Item label="Description" name="description">
+                    <Input placeholder="Add Description" />
+                </Form.Item>
+                <Form.Item label="Date" name="date">
+                    <Input type="date" />
+                </Form.Item>
+                <div className="d-flex justify-content-end">
+                    <Button type="submit" className="btn btn-primary">
+                        SAVE</Button>
+                </div>
+
             </Form>
 
         </Modal>
